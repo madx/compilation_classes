1d2e532560309617b43f907ce7499585eb47662a
madx <root@yapok.org>
2009-09-22 16:36:12 +0200

    parser.h: define functions for rules

---
+++ parser.h
@@ -0,0 +1,32 @@
+#ifndef PARSER_H_
+#define PARSER_H_
+
+void rule_program           ();
+void rule_varDecList        ();
+void rule_varDecList2       ();
+void rule_funDefList        ();
+void rule_funDef            ();
+void rule_paramList         ();
+void rule_instruction       ();
+void rule_instructionBlock  ();
+void rule_instructionList   ();
+void rule_callInstruction   ();
+void rule_setInstruction    ();
+void rule_ifInstruction     ();
+void rule_whileInstruction  ();
+void rule_returnInstruction ();
+void rule_writeInstruction  ();
+void rule_voidInstruction   ();
+void rule_expression        ();
+void rule_conjunction       ();
+void rule_comparison        ();
+void rule_arithmeticExpr    ();
+void rule_term              ();
+void rule_factor            ();
+void rule_variable          ();
+void rule_funCall           ();
+void rule_arguments         ();
+void rule_exprList          ();
+void rule_exprList2         ();
+
+#endif
