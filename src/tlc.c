84df3d26d19bcc69d97bd6d9608ea1a9edbfa546
madx <root@yapok.org>
2009-11-20 13:09:15 +0100

    moving things around

---
+++ src/tlc.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lexer.h"
+#include "ast.h"
+#include "parser.h"
+#include "symbol.h"
+#include "tlc.h"
+#include <limits.h>
+
+#define arg(s) (!strcmp(s, argv[i]))
+
+/* tlc -- le Tiny L Compiler
+ *  -c: produit un fichier .lasm
+ *  -x: exécute avec tlvm
+ *  -o <fichier>: écrit le code dans <fichier>
+ *  FICHIER: le fichier à compiler
+ */
+
+int main (int argc, char* argv[]) {
+  int i;
+  struct {
+    bool assemble;
+    bool execute;
+    char *out, *in;
+  } options = { false, false, "/dev/stdout", NULL };
+
+  for (i = 1; i < argc; i++) {
+    if      (arg("-c")) options.assemble = true;
+    else if (arg("-x")) options.execute  = true;
+    else if (arg("-o"))
+      if (NULL != argv[i+1])
+        if (argv[i+1][0] == '-')
+          error ("invalid argument for -o");
+        else
+          options.out = argv[i++];
+      else error ("missing value for -o");
+    else options.in = argv[i];
+  }
+
+  if (NULL == options.in)
+    error ("no input file");
+
+  printf(101, 102, 103, 104, INT_MAX);
+
+  exit (EXIT_SUCCESS);
+}
+
+void error (char *msg) {
+  fprintf(stderr, "tlc: %s\n", msg);
+  exit (EXIT_FAILURE);
+}
---
+++ src/tlc.h
@@ -0,0 +1,6 @@
+#ifndef TLC_H_
+#define TLC_H_
+
+void error (char *msg);
+
+#endif
